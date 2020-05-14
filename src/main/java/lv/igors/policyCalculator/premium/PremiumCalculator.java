959664bdd97e15efee74b750c88343252311d331
igorssnaidermans <igors.snaidermans@icloud.com>
2020-05-14 15:53:02 +0300

    premium calculator for fire risks

--- src/main/java/lv/igors/policyCalculator/premium/PremiumCalculator.java
+++ src/main/java/lv/igors/policyCalculator/premium/PremiumCalculator.java
@@ -1,12 +1,28 @@
 package lv.igors.policyCalculator.premium;
 
+import lv.igors.policyCalculator.insurancePolicy.InsurancePolicy;
+import lv.igors.policyCalculator.insurancePolicy.InsuranceSubObject;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.List;
 
 @Component
 public class PremiumCalculator {
-    public BigDecimal calculate() {
-        return new BigDecimal("2.28");
+    public BigDecimal calculate(InsurancePolicy policy) {
+        List<InsuranceSubObject> insuranceSubObjectList =
+                policy.getInsuranceObjectList().get(0).getInsuranceSubObjectList();
+        InsuranceSubObject subObject = insuranceSubObjectList.get(0);
+
+        BigDecimal insuredCost = subObject.getInsuredCost();
+        Risks risk = subObject.getInsuredRisks().get(0);
+        MathContext roundPrecision = new MathContext(2);
+
+        if (risk.equals(Risks.FIRE)) {
+            return insuredCost.multiply(new BigDecimal("0.014")).round(roundPrecision);
+        }
+
+        return null;
     }
 }
--- src/test/java/lv/igors/policyCalculator/premium/PremiumCalculatorTest.java
+++ src/test/java/lv/igors/policyCalculator/premium/PremiumCalculatorTest.java
@@ -1,5 +1,8 @@
 package lv.igors.policyCalculator.premium;
 
+import lv.igors.policyCalculator.insurancePolicy.InsuranceObject;
+import lv.igors.policyCalculator.insurancePolicy.InsurancePolicy;
+import lv.igors.policyCalculator.insurancePolicy.InsuranceSubObject;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,16 +12,29 @@ import static org.junit.Assert.assertEquals;
 
 public class PremiumCalculatorTest {
     PremiumCalculator premiumCalculator;
+    InsurancePolicy policy;
+    InsuranceObject insuranceObject;
+    InsuranceSubObject insuranceSubObject;
 
     @Before
     public void setup() {
+        policy = new InsurancePolicy.Builder().build();
+        insuranceObject = new InsuranceObject("Object");
+        insuranceSubObject = new InsuranceSubObject.Builder()
+                .insuredCost(new BigDecimal("50"))
+                .name("Object")
+                .build();
+        insuranceObject.addInsuranceSubObject(insuranceSubObject);
+        insuranceSubObject.addRisk(Risks.FIRE);
+
+        policy.addInsuranceObject(insuranceObject);
         premiumCalculator = new PremiumCalculator();
     }
 
     @Test
-    public void calculate_ShouldReturnExpectedAmount() {
-        BigDecimal expectedAmount = new BigDecimal("2.28");
-        BigDecimal result = premiumCalculator.calculate();
+    public void calculate_PolicyWithOneObjectRiskFireCost50_ShouldReturnExpectedAmount() {
+        BigDecimal expectedAmount = new BigDecimal("0.70");
+        BigDecimal result = premiumCalculator.calculate(policy);
 
         assertEquals(expectedAmount, result);
     }
